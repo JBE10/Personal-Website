96a93fd13b7e94d13267d0c1330d6758cc195bf0
JBE10 <bautiespino@icloud.com>
2025-03-31 20:15:45 -0300

    fanicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Navbar } from "@/components/navbar"
+import Head from "next/head"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -18,21 +19,28 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
+                                     children,
+                                   }: Readonly<{
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+      <html lang="en" suppressHydrationWarning>
+      <Head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+
+      </Head>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <Navbar />
-          {children}
-        </ThemeProvider>
+      <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+      >
+        <Navbar />
+        {children}
+      </ThemeProvider>
       </body>
-    </html>
-  )
+      </html>
+  );
 }
-
-
 
