26a63d9021bf16ebc4b4544509ca6013aa38b86b
JBE10 <bautiespino@icloud.com>
2025-04-07 20:33:51 -0300

    vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,45 +4,28 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { Navbar } from "@/components/navbar"
-import Head from "next/head"
-
-import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Juan Bautista Espino | Backend Developer & Cyber Analyst",
-  description:
-    "Portfolio of a Backend Developer and Cyber Analyst specializing in cybersecurity and system optimization",
-  icons: {
-    icon: "/favicon.ico",
-
-  },
+    title: "Juan Bautista Espino | Backend Developer & Cyber Analyst",
+    description:
+        "Portfolio of a Backend Developer and Cyber Analyst specializing in cybersecurity and system optimization",
 }
 
 export default function RootLayout({
-                                     children,
+                                       children,
                                    }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-      <html lang="en" suppressHydrationWarning>
-      <Head>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-
-      </Head>
-      <body className={inter.className}>
-      <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-      >
-        <Navbar />
-        {children}
-      </ThemeProvider>
-      </body>
-      </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            <Navbar />
+            {children}
+        </ThemeProvider>
+        </body>
+        </html>
+    )
 }
-
